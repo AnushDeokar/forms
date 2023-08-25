1db88e9ab166c2418638c520111d4f103edc6e0f
AnushDeokar <deokaranush@gmail.com>
2023-08-26 01:13:24 +0530

    updated auth middleware

--- backend/src/middleware/authverify.ts
+++ backend/src/middleware/authverify.ts
@@ -1,5 +1,6 @@
 import jwt, { Secret } from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
+import { IGetUserAuthInfoRequest } from '../../custom';
 
 export default function authverify(req: Request, res: Response, next: NextFunction) {
   try {
@@ -7,9 +8,9 @@ export default function authverify(req: Request, res: Response, next: NextFuncti
     const jwtSecret: Secret = process.env.JWT_SECRET as string;
     const decodedToken: any = jwt.verify(token as string, jwtSecret);
     const useremail: string = decodedToken.email;
-    
+
     if (token) {
-      req.user_email = useremail;
+      (req as IGetUserAuthInfoRequest).user_email = useremail; // Cast req to IGetUserAuthInfoRequest
       next();
     } else {
       res.status(401).json({
@@ -24,3 +25,4 @@ export default function authverify(req: Request, res: Response, next: NextFuncti
     });
   }
 };
+
